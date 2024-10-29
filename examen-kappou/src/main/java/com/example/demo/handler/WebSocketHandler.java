d5709112bdfe63345be18d9730d284852bdc12ef
gasparkappou <gasparkappou@gmail.com>
2024-10-28 21:23:43 -0300

    examen

---
+++ examen-kappou/src/main/java/com/example/demo/handler/WebSocketHandler.java
@@ -0,0 +1,36 @@
+package com.example.demo.handler;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private List<WebSocketSession> sessions = new ArrayList<>();
+    private List<String> mensajes = new ArrayList<String>();
+    
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        // Cuando se establece una conexión, agregar la sesión a la lista
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // Cuando se recibe un mensaje, enviarlo a todos los clientes conectados
+        for (WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(new TextMessage(message.getPayload()));
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+        // Eliminar la sesión cuando se cierra la conexión
+        sessions.remove(session);
+    }
+}
+
+
